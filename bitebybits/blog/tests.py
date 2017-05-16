4fedfe6ec5c4d45579c5fbd6b138b9e7a0199d6e
casol <casoltys@gmail.com>
2017-05-16 23:53:50 +0200

    Create test for post list

--- bitebybits/blog/tests.py
+++ bitebybits/blog/tests.py
@@ -1,9 +1,50 @@
 from django.test import TestCase, LiveServerTestCase, Client
 from django.utils import timezone
+# from django.core.urlresolvers import reverse  # Deprecated since version 1.10
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 from blog.models import Post
 
+
+class PostListTest(TestCase):
+    """Testing post list front page."""
+    def setUp(self):
+        self.c = Client()
+        self.user = User.objects.create_user(username="test", email="test@test.com", password="test")
+
+    def test_post_list_access(self):
+        """Get a page which shows all blog post at url/blog/."""
+        response = self.c.get(reverse('blog:post_list'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_list_custom_managers(self):
+        """Testing custom manager methods. Create two entries one with status published
+         and another with status draft."""
+        Post.objects.create(title='Test', body='Test', author=self.user, status='published')
+        Post.objects.create(title='Test', body='Test', author=self.user, status='draft')
+        self.assertEqual(Post.objects.count(), 2)
+        self.assertEqual(Post.published.count(), 1)
+
+    def test_post_list_template_context(self):
+        """Only published entries should be shown on list post page."""
+        # create few blog entries
+        Post.objects.create(title='Test', slug='test', body='Test', author=self.user,
+                            status='draft',
+                            created=timezone.now())
+        Post.objects.create(title='Test', slug='test', body='Test', author=self.user,
+                            status='draft',
+                            created=timezone.now())
+        Post.objects.create(title='Test', slug='test', body='Test', author=self.user,
+                            created=timezone.now(),
+                            status='published')
+
+        response = self.c.get(reverse('blog:post_list'))
+        # assert that context contains only published entries
+        self.assertEqual(len(response.context['posts']), 1)
+
+
+    #def test_
 
 class PostTest(TestCase):
     def test_create_post(self):
@@ -13,12 +54,13 @@ class PostTest(TestCase):
         post.body = 'You have to test'
         post.slug = 'test-post-post-test'
         post.created = timezone.now()
+
         # Create user
-        u = User(1)
-        u.save()
-        post.author = u
+        user = User.objects.create_user(username="test", email="test@test.com", password="test")
+        post.author = user
         # Save it
         post.save()
+
         all_posts = Post.objects.all()
         self.assertEqual(len(all_posts), 1)
         only_post = all_posts[0]
@@ -42,8 +84,8 @@ class AdminTest(LiveServerTestCase):
         c = Client()
 
         response = c.get('/admin/', fallow=True)
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue('Log in' in response.content)
+        self.assertEqual(response.status_code, 302)
+        #self.assertTrue('Log in' in response.content)
 
 
 class SmokeTest(TestCase):
